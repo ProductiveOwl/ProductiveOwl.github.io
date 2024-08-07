0a5a12f2a9c8530b4963072a66f54c3838492c52
Productive Owl <madhurika.madhu11@gmail.com>
2024-08-06 23:41:03 -0400

    Filter buttons work

--- projects.js
+++ projects.js
@@ -1,39 +1,30 @@
-//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
-//https://dev.to/dhintz89/simple-filters-in-css-or-js-185k
+/*
+The following websites were referenced when coding the filters:
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
+https://dev.to/dhintz89/simple-filters-in-css-or-js-185k
+*/
 
-function filterLanguage (e) {
-    const languages = document.querySelectorAll(".container");
-    let filter = e.target.dataset.filter;
-    if (filter === '*') {
+function filterLanguage(e) {
+    const languages = document.querySelectorAll(".container"); //Get all the project tiles
+    const languageButtons = document.querySelectorAll(".btn"); //Get all the language buttons
+
+    let filter = e.target.dataset.filter; //Find which filter to use for every project
+
+    if (filter === '*') { //Display all projects
         languages.forEach(language => language.classList.remove('hidden'));
     } else {
+        //For every project, if it contains the language selected, remove the hidden class. Otherwise, add the hidden class
         languages.forEach(language => {
             language.classList.contains(filter) ?
-            language.classList.remove('hidden') :
-            language.classList.add('hidden');
+                language.classList.remove('hidden') :
+                language.classList.add('hidden');
         });
     };
 
-    //refreshActiveLanguages(e);
-};
-
-/*function refreshLanguageButtons(e) {
-    const languageButtons = document.querySelectorAll(".btn");
+    //For every language button, if it is the button clicked, add the active class. Otherwise, remove it
     languageButtons.forEach(langButton => {
-        langButton.classList.remove('active');
+        langButton.classList.contains(filter) ?
+            langButton.classList.add('activeBtn') :
+            langButton.classList.remove('activeBtn');
     });
-    e.classList.add('active');
-};*/
-
-/*function refreshActive() {
-    // Add active class to the current button (highlight it)
-    var btnContainer = document.getElementById("allButtons");
-    var btns = btnContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function () {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
-    }
-}*/
+};
