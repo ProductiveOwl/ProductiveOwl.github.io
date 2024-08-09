2997670a12be32e367fd55cafcc21c9c76b0839d
Productive Owl <madhurika.madhu11@gmail.com>
2024-08-08 23:02:54 -0400

    Updated navigation bar for all of the main pages

--- projects.js
+++ projects.js
@@ -28,3 +28,13 @@ function filterLanguage(e) {
             langButton.classList.remove('activeBtn');
     });
 };
+
+/* Functionality when clicking on the menu icon and displaying the correct navigation bar*/
+function toggleResponsive() {
+    const showMenu = document.querySelectorAll(".NavBar")[0];
+    if (showMenu.classList.contains('responsive')) {
+        showMenu.classList.remove('responsive');
+    } else {
+        showMenu.classList.add('responsive')
+    }
+}
