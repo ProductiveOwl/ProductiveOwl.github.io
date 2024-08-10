414ba4563ed58743918f97a45e00acc91764bf93
ProductiveOwl <76631397+ProductiveOwl@users.noreply.github.com>
2024-08-09 23:31:12 -0400

    Update design (#19)
    * Reorganized files and updated Counter
    
    * Delete CLogo.svg
    
    * Delete CodeBlock.svg
    
    * Delete SQLiteLogo.svg
    
    * Delete coffeeIcon.svg
    
    * Delete cssIcon.svg
    
    * Delete gitHubLogo.png
    
    * Delete htmlIcon.svg
    
    * Delete javascriptIcon.svg
    
    * Delete menuIcon.svg
    
    * Delete terminalIcon.svg
    
    * Delete pythonLogo.svg

--- JavaScript/Counter/Counter.js
+++ JavaScript/Counter/Counter.js
@@ -1,18 +1,25 @@
-//Find number and buttons
-let current_num = document.getElementById("real_num");
-let increase = document.getElementById("increase");
-let reset = document.getElementById("reset");
-let decrease = document.getElementById("decrease");
+//Functionality when clicking on the menu icon and displaying the correct navigation bar
+function toggleResponsive() {
+  const showMenu = document.querySelectorAll(".NavBar")[0];
+  if (showMenu.classList.contains('responsive')) {
+      showMenu.classList.remove('responsive');
+  } else {
+      showMenu.classList.add('responsive')
+  }
+}
 
 //Execute a function when a button is pushed
-increase.onclick = function () {
+function increase () {
+  let current_num = document.getElementById("currentNum");
   current_num.innerHTML = Number(current_num.innerHTML) + 1;
 };
 
-reset.onclick = function () {
+function reset () {
+  let current_num = document.getElementById("currentNum");
   current_num.innerHTML = 0;
 }
 
-decrease.onclick = function () {
+function decrease () {
+  let current_num = document.getElementById("currentNum");
   current_num.innerHTML = Number(current_num.innerHTML) - 1;
 };
