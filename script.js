06cb094034ef89dbeba3cf4f22976912f361c312
ProductiveOwl <76631397+ProductiveOwl@users.noreply.github.com>
2024-09-10 22:17:35 -0400

    Added First Coop Reflection (#22)

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 /* Functionality when clicking on the menu icon and displaying the correct navigation bar*/
 function toggleResponsive() {
-    const showMenu = document.querySelectorAll(".NavBar")[0];
-    if (showMenu.classList.contains('responsive')) {
-        showMenu.classList.remove('responsive');
-    } else {
-        showMenu.classList.add('responsive')
-    }
+  const showMenu = document.querySelectorAll(".NavBar")[0];
+  if (showMenu.classList.contains('responsive')) {
+    showMenu.classList.remove('responsive');
+  } else {
+    showMenu.classList.add('responsive')
+  }
 }
 
 /*https://www.w3schools.com/HOWTO/howto_js_typewriter.asp*/
 var i = 0;
-var txt = 'Hi! I\'m ProductiveOwl'; /* The text */
+var txt = 'Hi! I\'m Madhurika'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -21,21 +21,39 @@ function typeWriter() {
   }
 }
 
+/*var j = 0;
+var allSkills = ["HTML", "CSS", "JavaScript", "Java"];
+var skillSpeed = 100;
+var skill = null;
+
+function chooseSkill() {
+  skill = allSkills[(Math.floor(Math.random() * allSkills.length))];
+  console.log("Skill is " + skill);
+  j = 0; // Reset j for the new skill
+  skillWriter(removeSkill); // Start writing the new skill
+}
+
+function removeSkill(chooseSkill) {
+  while (j >= 0) {
+    var text = document.getElementById("skill").innerHTML;
+    text = text.substring(0, text.length - 1); // Remove one character
+    document.getElementById("skill").innerHTML = text;
+    j--;
+  }
+  setTimeout(chooseSkill, 1000); // Start a new skill after removing the old one
+}
+
+function skillWriter(removeSkill) {
+  if (j < skill.length) {
+    document.getElementById("skill").innerHTML += skill.charAt(j);
+    j++;
+    setTimeout(skillWriter, skillSpeed);
+  } else {
+    removeSkill(chooseSkill());
+  }
+}*/
+
 function highlightText() {
   const title = document.querySelectorAll("h1")[0];
   title.style.backgroundColor = "green";
 }
-
-/*https://www.w3schools.com/HOWTO/howto_js_typewriter.asp
-https://www.w3schools.com/HOWTO/howto_js_scrolldrawing.asp
-https://www.w3schools.com/HOWTO/howto_css_smooth_scroll.asp#section2
-https://www.w3schools.com/HOWTO/howto_css_bg_gradient_scroll.asp
-https://www.w3schools.com/HOWTO/howto_css_zoom_hover.asp */
-
-/*https://www.geeksforgeeks.org/how-to-draw-a-curved-edge-hexagon-using-css/*/
-
-
-/* Color schemes 
-21, 25, 26, 31?, 39,  - https://visme.co/blog/website-color-schemes/
-4, 9, 19, 20, 24 - https://graphicmama.com/blog/website-color-schemes/
-*/
